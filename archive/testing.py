20227bce7fe4e54e0af42342a34ede4e8ee34f16
Alfonso Melchor <124454201+alfmelchor@users.noreply.github.com>
2023-05-11 15:52:28 -0700

    Delete archive directory

--- archive/testing.py
+++
@@ -1,9 +0,0 @@
-import customtkinter as ctk
-
-root = ctk.CTk()
-ctk.set_default_color_theme('/Users/alfonsomelchor/Desktop/Code Files/StudyAid/source/themes/green.json')
-
-switch = ctk.CTkSwitch(root)
-switch.pack()
-
-root.mainloop()
